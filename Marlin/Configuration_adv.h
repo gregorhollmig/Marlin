1c889ecd7c3b2d1cda9d328c5bf9763ab27cb6ec
Gregor Hollmig <gregor@hollmig-online.de>
2018-04-26 19:18:57 +0200

    Fixed Steps per Unit to Matthias

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1020,7 +1020,7 @@
  * in your `pins_MYBOARD.h` file. (e.g., RAMPS 1.4 uses AUX3 pins `X_CS_PIN 53`, `Y_CS_PIN 49`, etc.).
  * You may also use software SPI if you wish to use general purpose IO pins.
  */
-#define HAVE_TMC2130
+//#define HAVE_TMC2130
 #if ENABLED(HAVE_TMC2130)  // Choose your axes here. This is mandatory!
   #define X_IS_TMC2130
   //#define X2_IS_TMC2130
